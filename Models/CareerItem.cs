a569ad3726a2aa8e4560fe5566f8bf574a6962a9
Cristiano Andrade <cristianocontabil@gmail.com>
2022-03-26 18:04:58 -0300

    inicial

---
+++ Models/CareerItem.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BaltaDataAccess.Models
+{
+    public class CareerItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Course Course { get; set; }
+    }
+}
+
